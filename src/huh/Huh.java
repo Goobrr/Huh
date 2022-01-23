38cdccc54355f2225ebeaa03e47a96612046f231
Goobrr <ggoobbrr@gmail.com>
2022-01-23 20:01:29 +0700

    things went from 0 to 100 real quick.

--- src/huh/Huh.java
+++ src/huh/Huh.java
@@ -2,6 +2,7 @@ package huh;
 
 import arc.*;
 import arc.graphics.*;
+import arc.graphics.g2d.*;
 import arc.util.*;
 
 public class Huh implements ApplicationListener{
@@ -10,6 +11,15 @@ public class Huh implements ApplicationListener{
         Log.info("GL Version: @", Core.graphics.getGLVersion());
         Log.info("Max texture size: @", Gl.getInt(Gl.maxTextureSize));
         Log.info("Using @ Context", Core.gl30 != null ? "OpenGL 3" : "OpenGL 2");
+
+        //Core.camera = new Camera();
+    }
+
+    @Override
+    public void update(){
+        Core.graphics.setTitle("Huh? - " + Core.graphics.getFramesPerSecond() + "FPS");
+
+        //Draw.rect();
     }
 
     @Override
