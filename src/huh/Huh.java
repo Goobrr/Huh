c5379f0d477afe42a9710de877b6baf1ffcbba92
Goobrr <ggoobbrr@gmail.com>
2022-01-23 19:49:42 +0700

    Let there be a 5:4 window

--- src/huh/Huh.java
+++ src/huh/Huh.java
@@ -1,9 +1,19 @@
 package huh;
 
+import arc.*;
+import arc.graphics.*;
 import arc.util.*;
 
-public class Huh {
-    public static void main(String[] args){
-        Log.info("Huh");
+public class Huh implements ApplicationListener{
+    @Override
+    public void init(){
+        Log.info("GL Version: @", Core.graphics.getGLVersion());
+        Log.info("Max texture size: @", Gl.getInt(Gl.maxTextureSize));
+        Log.info("Using @ Context", Core.gl30 != null ? "OpenGL 3" : "OpenGL 2");
+    }
+
+    @Override
+    public void exit(){
+        Log.info("Goodbye");
     }
 }
---
+++ src/huh/Launcher.java
@@ -0,0 +1,16 @@
+package huh;
+
+import arc.backend.sdl.*;
+
+public class Launcher {
+    public static void main(String[] args){
+        new SdlApplication(new Huh(), new SdlConfig(){{
+            title = "Funny";
+
+            width = 720;
+            height = 576;
+        }});
+    }
+
+
+}
