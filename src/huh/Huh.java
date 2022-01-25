e859b737185f0c591a0dab89278eee4ba21cbd98
Goobrr <ggoobbrr@gmail.com>
2022-01-25 09:53:21 +0700

    ApplicationCore

--- src/huh/Huh.java
+++ src/huh/Huh.java
@@ -2,24 +2,24 @@ package huh;
 
 import arc.*;
 import arc.graphics.*;
-import arc.graphics.g2d.*;
 import arc.util.*;
+import huh.core.*;
 
-public class Huh implements ApplicationListener{
+public class Huh extends ApplicationCore{
     @Override
-    public void init(){
+    public void setup(){
         Log.info("GL Version: @", Core.graphics.getGLVersion());
         Log.info("Max texture size: @", Gl.getInt(Gl.maxTextureSize));
         Log.info("Using @ Context", Core.gl30 != null ? "OpenGL 3" : "OpenGL 2");
 
-        //Core.camera = new Camera();
+
+        add(new Renderer());
+        add(new UI());
     }
 
     @Override
     public void update(){
         Core.graphics.setTitle("Huh? - " + Core.graphics.getFramesPerSecond() + "FPS");
-
-        //Draw.rect();
     }
 
     @Override
---
+++ src/huh/core/Renderer.java
@@ -0,0 +1,10 @@
+package huh.core;
+
+import arc.*;
+import arc.graphics.*;
+
+public class Renderer implements ApplicationListener{
+    public Renderer(){
+        Core.camera = new Camera();
+    }
+}
